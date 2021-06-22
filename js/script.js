155d5044df73703adab35f5e2b261a515fcc1e19
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-22 15:49:22 +0200

    back, next

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const app = new Vue({
     el: "#app",
     data: {
-
+        photos: [
+            "https://source.unsplash.com/BP2RioglKXk/1600x900", 
+            "https://source.unsplash.com/WYmPNSUyfNg/1600x900",
+            "https://source.unsplash.com/3dUpL1h97CQ/1600x900",
+            "https://source.unsplash.com/yn97LNy0bao/1600x900"
+        ],
+        counter: 0
     },
     methods: {
+        next() {
+            (this.counter == this.photos.length - 1) ? this.counter = 0 : this.counter++;
+            console.log(this.counter);
+        },
+        back() {
+            (this.counter == 0) ? this.counter = this.photos.length -1 : this.counter--;
+            console.log(this.counter);
+        }
 
     }
-})
+});
